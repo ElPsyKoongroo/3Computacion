9a43673462933ac7c981a8acd7248e5411cdfbc3
sergious234 <sergious234@gmail.com>
2023-03-13 00:07:24 +0100

    SP videos/imagenes

--- SP/Practica3/Practica3.m
+++ SP/Practica3/Practica3.m
@@ -211,3 +211,66 @@ close all;
 
     stop(video);
     close all; 
+    
+%% Guardar fotitos
+
+stop(video);
+clc, clear;
+video = videoinput('linuxvideo',1,'RGB24_320x240');
+video.TriggerRepeat = inf;
+
+% preview(video)
+start(video)
+preview(video)
+pause(2);
+for i = 1:15
+    I = getsnapshot(video);
+    imwrite(I, ("ejemplos"+i+".jpg"));
+    pause(2);
+end
+stop(video)
+close all;
+closepreview(video);
+
+%% Guardar video
+stop(video)
+
+clc, clear;
+video = videoinput('linuxvideo',1,'RGB24_320x240');
+aviobj = VideoWriter("Videito.avi");
+aviobj.FrameRate = 15;
+video.TriggerRepeat = inf;
+video.FrameGrabInterval = 2;
+
+set(video, 'LoggingMode', 'memory');
+open(aviobj)
+start(video)
+preview(video);
+pause(2);
+while (video.FramesAcquired < 150)
+    I = getdata(video, 1);
+    writeVideo(aviobj, I);
+end
+stop(video)
+close(aviobj)
+closepreview(video);
+
+%% Visualizar fotitos
+clc, clear;
+
+figure(), hold on;
+for i = 1:10
+    I = imread("whisky"+i+".jpg");
+    imshow(I);
+    pause(1);
+end
+
+close all;
+
+
+
+
+
+
+
+
