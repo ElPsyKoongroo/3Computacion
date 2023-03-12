7cd0f011cc8da332665da6a35bd13a78919ab712
sergious234 <sergious234@gmail.com>
2023-03-12 13:18:44 +0100

    Ejercicios practica 3

--- SP/Practica3/Practica3.m
+++ SP/Practica3/Practica3.m
@@ -16,11 +16,6 @@ I = double (imresize(I_Original, 0.33));
 
 %}
 I_media2 = (I(:,:,1)+I(:,:,2)+I(:,:,3)) / 3;
-
-
-
-
-
 umbrales = [70,140,210];
 I_Umbrales = uint8(zeros([n_rows, n_cols, 3, 3]));
 
@@ -34,7 +29,6 @@ for i = 1:3
     subplot(2,2,i+1), imshow((I_Umbrales(:,:,:, i) + uint8(I))), hold on;
 end
 close all;
-%% Ejercicio 2
 
 %{
     2.- Para cada una de las imágenes generadas en el apartado anterior:
@@ -48,12 +42,14 @@ figure();
 for i = 1:3 
     temp = I_Umbrales(:,:,:,i);
     Ib = temp(:,:,1) == 255;
-    [IEtiq, N] = Etiquetar(Ib);
+    [IEtiq, N] = bwlabel(Ib);
     
+
+    stats = regionprops(IEtiq, 'Area', 'Centroid');
     new_image = zeros(n_rows, n_cols, 3);
-    areas = funcion_calcula_areas(IEtiq, N);
-    centroides = funcion_calcula_centroides(IEtiq, N);
-    [sorted_areas, indexes] = sort(areas, 'descend');
+    % areas = funcion_calcula_areas(IEtiq, N);
+    centroides = cat(1, stats.Centroid);
+    [sorted_areas, indexes] = sort(cat(1, stats.Area), 'descend');
 
     for j = 1:5
         Color = round(255*rand(1,3)); 
@@ -69,3 +65,149 @@ for i = 1:3
     
 end
 
+%% Segunda parte
+
+%% Ejercicio 3
+%{
+    3.- La escena inicialmente oscurecida y aclarándose progresivamente (utilizar la instrucción
+        imadjust y valores de gamma entre 0 y 4 con pasos de 0.05).
+%}
+
+clc, clear;
+imaqhwinfo('linuxvideo');
+video = videoinput('linuxvideo',1,'RGB24_320x240');
+
+video.TriggerRepeat = inf;
+video.FrameGrabInterval = 2;
+start(video);
+
+TIEMPO=[];
+figure(), hold on;
+Gamma = 4;
+while (Gamma > 0)
+    [I, TIME, METADATA]=getdata(video,1);
+    TIEMPO = [TIEMPO ; TIME METADATA.AbsTime];
+    J = imadjust(I, [], [], Gamma);
+    imshow(J);
+    Gamma = Gamma - 0.05;
+end
+stop(video)
+close all;
+
+%% Ejercicio 4
+%{
+    4.- Todos los píxeles que tengan una intensidad mayor que un determinado umbral. Asignar
+        inicialmente el valor 0 a este umbral e ir aumentándolo progresivamente con pasos de unidad
+        hasta el 255.
+%}
+
+clc, clear;
+imaqhwinfo('linuxvideo');
+video = videoinput('linuxvideo',1,'RGB24_320x240');
+
+video.TriggerRepeat = inf;
+video.FrameGrabInterval = 2;
+start(video);
+
+Umbral = 0;
+
+figure(), hold on;
+while (Umbral < 255)
+    I = getdata(video,1);
+    I(I < Umbral) = 0;
+    imshow(I);
+    Umbral = Umbral + 1;
+    % I(1:50, 1:50, :)
+end
+stop(video)
+close all;
+
+%% EJERCICIO 5
+%{
+    5.1.- Las diferencias que se producen entre los distintos frames de intensidad que captura la
+    webcam (utilizar la instrucción imabsdiff para hacer la diferencia entre el frame actual y el
+    adquirido previamente).
+%}
+
+clc, clear;
+
+video = videoinput('linuxvideo',1,'RGB24_320x240');
+video.ReturnedColorSpace = 'grayscale';
+video.TriggerRepeat = inf;
+video.FrameGrabInterval = 1;
+start(video);
+
+figure(), hold on;
+I1 = getdata(video, 1);
+while ( video.FramesAcquired < 150 )
+    I2 = getdata(video,1);
+    imshow(imabsdiff(I1, I2));
+    I1 = getdata(video,1);
+end
+stop(video)
+close all;
+
+%% Ejercicio 5.2
+%{
+    Los píxeles cuyas diferencias de intensidad son significativas (considerar un umbral de
+    100 para establecer de diferencias de intensidad significativas).
+%}
+    clc, clear;
+    datos = imaqhwinfo('linuxvideo');
+    video = videoinput('linuxvideo',1,'RGB24_320x240');
+    video.TriggerRepeat = inf;
+    video.ReturnedColorSpace = 'grayscale';
+
+    video.FrameGrabInterval = 2;
+    start(video);
+    
+    figure(), hold on;
+    I1 = getdata(video, 1);
+    
+    figure(), hold on;
+    while ( video.FramesAcquired < 100 )
+        I2 = getdata(video,1);
+        i_intensidad = imabsdiff(I1, I2);
+        I1(i_intensidad < 100) = 0;
+        imshow(I1);
+        I1 = getdata(video,1);
+    end
+    stop(video)
+    close all;
+
+    %% EJERCICIO 5.3
+    %{ 
+        El seguimiento de la agrupación mayor de píxeles que presenta 
+        una diferencia de intensidad significativa. El seguimiento debe 
+        visualizarse a través de un punto rojo situado en el centroide 
+        de la agrupación. 
+    %}
+
+    clc, clear;
+    video = videoinput('linuxvideo',1,'RGB24_320x240');
+    video.TriggerRepeat = inf;
+    video.ReturnedColorSpace = 'grayscale';
+
+    video.FrameGrabInterval = 2;
+    start(video);
+    
+    figure(), hold on;
+    I1 = getdata(video, 1);
+
+    while ( video.FramesAcquired < 150 )
+        I2 = getdata(video,1);
+        Ib = imabsdiff(I1, I2) > 100;
+        [IEtiq, N] = bwlabel(Ib);
+        stats = regionprops(IEtiq, 'Area', 'Centroid');
+        [sorted_areas, indexes] = sort(cat(1,stats.Area), 'descend');
+        centroides = cat(1, stats.Centroid);
+        imshow(I1), hold on, 
+        len = size(indexes);
+        if ( len > 0 ) & (indexes(1) ~= 0)
+            plot(centroides(indexes(1),1), centroides(indexes(1),2), '*r');
+        end
+        I1 = getdata(video, 1);
+    end
+
+    stop(video);
+    close all; 
