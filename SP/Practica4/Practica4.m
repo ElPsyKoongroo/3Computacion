3daf781fffcd44b2ae2c7eb61f1fe288415e8b75
sergious234 <sergious234@gmail.com>
2023-05-04 02:16:17 +0200

    otsu

--- SP/Practica4/Practica4.m
+++ SP/Practica4/Practica4.m
@@ -1,6 +1,7 @@
-I = double(imread('A1.jpg'));
+I = double(imread('A3.jpg'));
 
 I_intensidad = uint8((I(:,:,1)+I(:,:,2)+I(:,:,3)) / 3);
-[counts, ~] = imhist(imread('A1.jpg'));
-a = otsuthresh(counts)*255
-b = funcion_otsu(counts)
+    [counts, ~] = imhist(imread('P4.jpg'));
+    MATLAB = otsuthresh(counts)*255
+    MAIRENA = funcion_otsu(counts)
+    WIKIPEDIA = funcion_otsu_wiki(counts)
