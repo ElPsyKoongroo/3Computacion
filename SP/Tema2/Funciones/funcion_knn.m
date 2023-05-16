71f7dc7fde6a6a1d06e73ab03c82ea9e0c2f3a90
sergious234 <sergious234@gmail.com>
2023-05-16 22:03:20 +0200

    Knn distancias

--- SP/Tema2/Funciones/funcion_knn.m
+++ SP/Tema2/Funciones/funcion_knn.m
@@ -1,14 +1,15 @@
 function [YTest] = funcion_knn(XTest, XTrain, YTrain, k)
 
-    [num_muestras, num_descriptores] = size(XTest_oi);
-    
+    [num_muestras_test, num_descriptores] = size(XTest_oi);
     [num_muestras_train, num_descriptores_train] = size(XTrain_oi);
-    distancias = repmat(XTrain_oi, num_muestras, 1);
-    
 
-    for i = 1:num_muestras_train
+    copia_XTRAIN = repmat(XTrain_oi, num_muestras_test, 1);
+    
+    distancias = zeros(num_muestras_test, num_muestras_train);
+    for i = 1:num_muestras_test
         rango = ((i-1)*num_muestras_train)+1:i*num_muestras_train;
-        distancias(rango, :) - XTest_oi(i, :);
+        distancias_act = (copia_XTRAIN(rango, :) - XTest_oi(i, :)) .^2;
+        distancias(i, :) = sum(distancias_act');
     end
 end
 
